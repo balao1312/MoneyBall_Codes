73ee457fe0976de7ba900fa288816229dd34141c
balao1312 <balao1014@hotmail.com>
2020-06-28 04:24:19 +0800

    1

--- use_of_Elasticsearch.py
+++ use_of_Elasticsearch.py
@@ -16,12 +16,12 @@ print('ES object : ', es)
 es.indices.create(index='test-index', ignore=400)
 
 # 刪除一個 index
-#es.indices.delete(index='test-index', ignore=400)
+# es.indices.delete(index='test-index', ignore=400)
 
 # 搜尋 & 用 json 去 dump 出來
-data = es.search(index='twinttweets',size=100)
-jj = json.dumps(data,indent=2)
-print('jj : \n',jj)
+data = es.search(index='twinttweets', size=100)
+jj = json.dumps(data, indent=2)
+print('jj : \n', jj)
 
 print(type(data))
 
@@ -30,7 +30,7 @@ tweet_data = []
 for ii in data:
     print(ii)  # took, timed_out, _sahrds, hits
 print(len(data['hits']['hits']))
-for ii,content in enumerate(data['hits']['hits']):
+for ii, content in enumerate(data['hits']['hits']):
     try:
         print(f'{"=" * 50} \n{ii} : \n{"-" * 50}')
         print(content["_source"]["name"])
@@ -43,15 +43,15 @@ for ii,content in enumerate(data['hits']['hits']):
         print(content["_source"]["tweet"])
 
         tweet_data.append([
-                            content["_source"]["name"],
-                            content["_source"]["id"],
-                            content["_source"]["date"],
-                            content["_source"]["link"],
-                            content["_source"]["nlikes"],
-                            content["_source"]["nreplies"],
-                            content["_source"]["nretweets"],
-                            content["_source"]["tweet"],
-                           ])
+            content["_source"]["name"],
+            content["_source"]["id"],
+            content["_source"]["date"],
+            content["_source"]["link"],
+            content["_source"]["nlikes"],
+            content["_source"]["nreplies"],
+            content["_source"]["nretweets"],
+            content["_source"]["tweet"],
+        ])
 
     except Exception as e:
         print(e)
@@ -61,9 +61,7 @@ df = pandas.DataFrame(tweet_data)
 df.columns = columns
 
 print(df)
-df.to_csv('ooo.csv',encoding='utf-8-sig')
-
-
+df.to_csv('ooo.csv', encoding='utf-8-sig')
 
 # 迴圈塞資料
 # # doc = {
@@ -76,12 +74,8 @@ df.to_csv('ooo.csv',encoding='utf-8-sig')
 #     es.create(index='test-index', id =i, body=doc)
 
 
-
 # 用 filter path 篩選結果
 # data = es.search(index='test', filter_path=['hits.hits._id', 'hits.hits._type'])
 # print(data)
 # for anyy in data['hits']['hits']:
 #     print(anyy)
-
-
-
